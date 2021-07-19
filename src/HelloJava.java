e9810819d268d78e596301eb6b86eec36312787e
yurim01 <tg3fnq41612g@naver.com>
2021-07-19 17:09:15 +0900

    Initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,15 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println(22);
+        System.out.println(6+16);
+
+        System.out.println("안녕 나는 문자열이야");
+        System.out.println('헐');
+        System.out.println("안녕 나는 \"문자열\"이야\n헐");
+
+        //  주석
+        /*
+            주석
+         */
+    }
+}
